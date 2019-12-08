664398623f1f6ab4aac01d7fd79c45971b3943fd
amberwilso1@my.smccd.edu <amberwilso1@my.smccd.edu>
2019-12-08 22:41:03 +0700

    Update search and login

--- Tulis Kode Disini aja ya!/Final.c
+++ Tulis Kode Disini aja ya!/Final.c
@@ -1,120 +1,180 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+#include <c++/bits/locale_classes.h>
 
-int n;
-
+int n = 0;
+int i, a;//untuk looping
 int id[10]; //array id
 char nabar[10][100]; //array nama barang
 int katbar[10]; //array kategory barang
 float harga[10]; //array harga
-char narak[5][1]={"A","B","C","D","E"};//inisialisasi nama rak
 int rak[10][5]; //array rak
 int jumbar[10]; //array jumlah barang
-int loginusr;
-char user,pass; // create user
-char userlog,passlog; //login user
 
-void login(){
-	printf ("Selamat datang di UPN Store Retail");
-	printf ("==================================");
-	printf ("1. Create User Admin.");
-	printf ("2. Login");
-	printf ("Masukkan Pilihan anda : ");
-	scanf  ("%d", &loginusr);
-	if (login == 1){
-		printf ("Masukkan Username anda : ");
-		scanf  ("%s", &user);
-		printf ("Masukkan Password anda : ");
-		scanf  ("%s", &pass);
-	}
-	else if (login == 2){
-		printf ("Masukkan Username anda : ");
-		scanf  ("%s", &userlog);
-		printf ("Masukkan Password anda : ");
-		scanf  ("%s", &passlog);
-		if (userlog = user && passlog = pass){
-			printf ("Login Telah berhasil.");
-		}
-		else {
-			printf ("Username dan password yang anda masukkan salah.");
-		}
-	}
-}
-void input(){
-    int d=0;
-    n=n++;
-    printf("\n%d",n);
-    printf("- Masukkan Id Barang        : ");
-    scanf("%d",&id[n]);
-    printf("- Masukkan Nama Barang     : ");
-    scanf("%s",&nabar[n][100]);
-    fflush(stdin);
-    printf("- Masukkan Kategori Barang \n  1.Makanan	3.ATK		5.Pakaian \n  2.Minuman	4.Elektronik \n  Pilih : ");
-    scanf("%d",&katbar[n]);
-    printf("\n%d",katbar[n]);
-    d=katbar[n];
-    printf("- Masukkan Harga Barang    : ");
-    scanf("%f",&harga[n]);
-    printf("\n%d",d);
-    printf("\n%d",d-2);
-    printf("\n%c",narak[d-2][1]);
-    printf("- Masukkan Barang Pada Rak %c \nPada Lantai : ",narak[d-1][1]);
-    scanf("%d",&rak[n][d]);
-    printf("- Masukkan Jumlah Barang   : ");
-    scanf("%d",&jumbar[n]);
+void input() {
+    printf ("\n%d", n);
+    printf ("- Masukkan Id Barang           : ");
+    scanf ("%d", &id[n]);
+    printf ("- Masukkan Nama Barang         : ");
+    scanf ("%s", &nabar[n][100]);
+    fflush (stdin);
+    printf ("- Masukkan Kategori Barang \n  1.Makanan	3.ATK		5.Pakaian \n  2.Minuman	4.Elektronik \n  Pilih : ");
+    scanf ("%d", &katbar[n]);
+    printf ("\n%d", katbar[n]);
+    printf ("- Masukkan Harga Barang        : ");
+    scanf ("%f", &harga[n]);
+    printf ("- Masukkan No Rak Untuk Barang : ");
+    scanf ("%d", &rak[n]);
+    printf ("- Masukkan Jumlah Barang       : ");
+    scanf ("%d", &jumbar[n]);
+    n++;
 }
 
-void update(){
-
-}
-
-void search(){
-
-}
-
-void view(){
-    int d=0;
-
-    printf("\n- Id Barang       : %d",id[n]);
-    printf("\n- Nama Barang     : %s",nabar[n][100]);
-    printf("\n- Kategori Barang : %d",katbar[n]);
-    d=katbar[n]-1;
-    printf("\n- Harga Barang    : %d",harga[n]);
-    printf("\n- Posisi Barang   : Rak-%c%d",narak[d][1],rak[n][d]);
-    printf("\n- Jumlah Barang   : %d",jumbar[n]);
-}
-
-int main(){
-    int y=1, pil;
-    while(y==1){
-        system("cls");
-        printf("\nAplikasi Kasir\n");
-        printf("=====================================\n");
-        printf("1.Input \n2.Update \n3.Search & View \n4.Sort & View\n");
-        printf("Pilih : ");
-        scanf("%d",&pil);
-
-        if(pil==1){
-            system("cls");
-            login();
-            input();
-        }
-        else if(pil==2){
-            system("cls");
-            update();
-            getch();
-        }
-        else if(pil==3){
-            system("cls");
-            search();
-            getch();
-        }
-        else{
-            system("cls");
-            view();
-            getch();
+void update() {
+    int scr;
+    int chg;
+    printf ("\nUbah Data Barang\n");
+    printf ("Masukkan Id Barang Yang Ingin Di Ubah : ");
+    printf ("%d", &scr);
+    for (i = 0; i < n; i++) {
+        if (id[i] == scr) {
+            printf ("Masukkan stok barang yang ingin diganti : ");
+            scanf ("%d", &chg);
+            jumbar[i] = chg[i]; //mengganti jumlah dari variable chg
+            printf ("Jumlah barang telah di update!");
+        } else {
+            printf ("Kamu memasukkan inputan yang salah ya.");
         }
     }
 }
 
+void search() {
+    int cari;
+    int cariid;
+    int cariktg;
+    printf ("Ayo Cari barang di toko ini.");
+    printf ("Cari barang berdasarkan apa nih ?");
+    printf ("1. Id Barang");
+    printf ("2. Kategori Barang");
+    printf ("Masukkan pilihan anda");
+    scanf ("%d",&cari);
+    if (cari == 1) {
+        printf("Masukkan id barang yang ingin dicari.");
+        scanf ("%d",&cariid);
+    }
+    else if (cari == 2) {
+        printf("Masukkan kategori barang yang ingin dicari.");
+        scanf ("%d",&cariktg);
+    }
+    else {
+        printf("Salah Inputan !!!");
+    }
+};
+
+void sortview() {
+    int pilsw, t;
+    printf ("Pilih Menampilkan Data Urut(asscending) Berdasarkan");
+    printf ("1. Id Barang \n2. Nama \n3. Kategori \n4. Harga \n5. No Rak \n6. Jumlah Barang");
+    printf ("Pilih : ");
+    scanf ("%d", &pilsw);
+
+    switch (pilsw) {
+        case 1 : //id barang
+            system ("cls");
+            for (i = 0; i < n; i++) {
+                for (a = i; a > 0 && id[a] < id[a - 1]; a--) {
+                    t = vid[a - 1];//swap id
+                    vid[a - 1] = id[a];
+                    id[a] = t;
+                    t = vnabar[a - 1];//swap nama
+                    vnabar[a - 1] = nabar[a];
+                    nabar[a] = t;
+                    t = vkatbar[a - 1];//swap kategori
+                    vkatbar[a - 1] = katbar[a];
+                    katbar[a] = t;
+                    t = vharga[a - 1];//swap harga
+                    vharga[a - 1] = harga[a];
+                    harga[a] = t;
+                    t = vrak[a - 1];//swap rak
+                    vrak[a - 1] = rak[a];
+                    rak[a] = t;
+                    t = vjumlah[a - 1];//swap jumlah
+                    vjumlah[a - 1] = jumlah[a];
+                    jumlah[a] = t;
+                }
+            }
+            for (i = 0; i < n; i++) {
+                view ()
+            }
+            getch ();
+            break;
+        case 2 : //nama barang
+            system ("cls");
+            getch ();
+            break;
+        case 3 : //kategori
+            system ("cls");
+            getch ();
+            break;
+        case 4 : //harga
+            system ("cls");
+            getch ();
+            break;
+        case 5 ://no rak
+            system ("cls");
+            getch ();
+            break;
+        case 6 ://jumlah barang
+            system ("cls");
+            getch ();
+            break;
+        default :
+            printf ("Eror");
+    }
+}
+
+void view() {
+    int no = i + 1;
+    printf ("\n%d. Id Barang       : %d", no, vid[n]);
+    printf ("\n   Nama Barang     : %s", vnabar[n][100]);
+    printf ("\n   Kategori Barang : %d", vkatbar[n]);
+    printf ("\n   Harga Barang    : %d", vharga[n]);
+    printf ("\n   No Rak Barang   : %d", vrak[n]);
+    printf ("\n   Jumlah Barang   : %d\n", vjumbar[n]);
+}
+
+int main() {
+    int y = 1, pil;
+    while (y == 1) {
+        system ("cls");
+        printf ("\nAplikasi Kasir\n");
+        printf ("=====================================\n");
+        printf ("1.Input \n2.Update \n3.Search & View \n4.Sort & View\n");
+        printf ("Pilih : ");
+        scanf ("%d", &pil);
+
+        switch (pil) {
+            case 1 :
+                system ("cls");
+                input ();
+                break;
+            case 2 :
+                system ("cls");
+                update ();
+                getch ();
+                break;
+            case 3 :
+                system ("cls");
+                search ();
+                getch ();
+                break;
+            case 4 :
+                view ();
+                getch ();
+                break;
+            default :
+                printf ("Menu Eror");
+        }
+    }
+}
